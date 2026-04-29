7a2cf5b441947b6e6a9337e879994783d577dbf0
Vladislav Antipin <156520208+Vladislav-Antipin@users.noreply.github.com>
2026-04-29 16:21:52 +0200

    start missing query robustness

--- src/ir_rpp/scores.py
+++ src/ir_rpp/scores.py
@@ -15,9 +15,30 @@ from .pref_eval.aggregation import rank_aggregation
 
 from .pref_eval.measures.measures import is_metric
 
+from copy import deepcopy
+
+
+def subsample_labels(runs, label_fraction):
+    runs = deepcopy(runs)
+    ...
+    return runs
+
+
+def subsample_queries(runs, query_fraction):
+    runs = deepcopy(runs)
+
+    if query_fraction < 1.0:
+        qids = list(next(iter(runs.values())).keys())
+        num_sample = max(int(len(qids) * query_fraction), 1)
+        if num_sample < len(qids):
+            qids_to_remove = random.sample(qids, len(qids) - num_sample)
+            for system in runs.keys():
+                for qid in qids_to_remove:
+                    runs[system].pop(qid, None)
+    return runs
+
 
 def evaluate_preferences(
-    qrels,
     runs,
     measures,
     measure_set=None,  # preferences, all, none
@@ -25,54 +46,69 @@ def evaluate_preferences(
     nosummary=False,
     query_fraction=1.0,
     label_fraction=1.0,
-    label_fraction_policy="random",  # random, pool TODO: no query_fraction_policy, is it taken in order then??
+    label_fraction_policy="random",  # random, pool TODO: why no query_fraction_policy
     samples=1,
     # topk=None,
 ) -> tuple[list, list | pd.DataFrame, list | pd.DataFrame]:
-    """Main of pref_eval.py adapted for usage in notebooks. Qrels and runs are loaded separately."""
+    """Main of pref_eval.py adapted for usage in notebooks. Runs are loaded separately."""
 
+    # runs = deepcopy(runs) # NOTE: his code may modify arguments
     measures = get_measures(measures, measure_set)
 
-    for sample in range(samples):
-        if query_fraction < 1.0:
-            qids = list(qrels.keys())
-            num_sample = max(int(len(qids) * query_fraction), 1)
-            if num_sample < len(qids):
-                qids_to_remove = random.sample(qids, len(qids) - num_sample)
-                for qid in qids_to_remove:
-                    qrels.pop(qid, None)
-        if label_fraction < 1.0:
-            if label_fraction_policy == "pool":
-                # qrels_pool_frequencies = trec_io.compute_qrel_pool_frequencies(
-                #     args.runfiles, qrels
-                # )
-                raise NotImplementedError(
-                    "we will not use pooled label fraction policy"
-                )
-            for qid in qrels.keys():
-                dids = list(qrels[qid].keys())
-                num_sample = max(int(len(dids) * label_fraction), 1)
-                if num_sample < len(dids):
-                    if label_fraction_policy == "pool":
-                        # dids_to_remove = qrels_pool_frequencies[qid][num_sample:]
-                        raise NotImplementedError(
-                            "we will not use pooled label fraction policy"
-                        )
-                    else:
-                        dids_to_remove = random.sample(dids, len(dids) - num_sample)
-                    for did in dids_to_remove:
-                        qrels[qid].pop(did, None)
+    full_summary = []
+    preferences = []
+    raw_metrics = []
 
-        summary, preferences, raw_metrics = get_prefs(
+    for sample in range(samples):
+        # NOTE: it wouldn't work since originally he does it while loading the data
+        # - he modifies qrels and then uses t to load runs
+        # if query_fraction < 1.0:
+        #     qids = list(qrels.keys())
+        #     num_sample = max(int(len(qids) * query_fraction), 1)
+        #     if num_sample < len(qids):
+        #         qids_to_remove = random.sample(qids, len(qids) - num_sample)
+        #         for qid in qids_to_remove:
+        #             qrels.pop(qid, None)        
+        # if label_fraction < 1.0:
+        #     if label_fraction_policy == "pool":
+        #         # qrels_pool_frequencies = trec_io.compute_qrel_pool_frequencies(
+        #         #     args.runfiles, qrels
+        #         # )
+        #         raise NotImplementedError(
+        #             "we will not use pooled label fraction policy"
+        #         )
+        #     for qid in qrels.keys():
+        #         dids = list(qrels[qid].keys())
+        #         num_sample = max(int(len(dids) * label_fraction), 1)
+        #         if num_sample < len(dids):
+        #             if label_fraction_policy == "pool":
+        #                 # dids_to_remove = qrels_pool_frequencies[qid][num_sample:]
+        #                 raise NotImplementedError(
+        #                     "we will not use pooled label fraction policy"
+        #                 )
+        #             else:
+        #                 dids_to_remove = random.sample(dids, len(dids) - num_sample)
+        #             for did in dids_to_remove:
+        #                 qrels[qid].pop(did, None)
+        
+        if query_fraction < 1.0:
+            runs = subsample_queries(runs, query_fraction)
+        
+        if label_fraction < 1.0:
+            # TODO: implement it...
+            raise NotImplementedError
+            runs = subsample_labels(runs, label_fraction)
+
+        summary_per_sample, preferences_per_sample, raw_metrics_per_sample = get_prefs(
             sample=sample,
             runs=runs,
             measures=measures,
             per_query=query_eval_wanted,
             output_df=False,
         )
-        full_summary = []
+        full_summary_per_sample = []
         if not nosummary:
-            for pair_tag, m_prefs in summary.items():
+            for pair_tag, m_prefs in summary_per_sample.items():
                 output_object = {}
                 output_object["qid"] = "all"
                 output_object["runi"] = pair_tag.split(":")[0]
@@ -80,9 +116,12 @@ def evaluate_preferences(
                 output_object["sample"] = sample
                 for measure, pref in m_prefs.items():
                     output_object[measure] = pref
-                full_summary.append(output_object)
+                full_summary_per_sample.append(output_object)
+        full_summary.extend(full_summary_per_sample)
+        preferences.extend(preferences_per_sample)
+        raw_metrics.extend(raw_metrics_per_sample)
 
-        return full_summary, preferences, raw_metrics
+    return full_summary, preferences, raw_metrics
 
 
 def prepare_prefs(prefs, metrics, current_sample, qids=None):
@@ -111,6 +150,7 @@ def prepare_prefs(prefs, metrics, current_sample, qids=None):
 
 
 def get_metrics_from_prefs(prefs, metrics, current_sample, qids=None):
+    print(prefs)
     retval: Metrics = {}
     for obj in prefs:
         qid: str = obj["qid"]
@@ -142,6 +182,7 @@ def aggregate_preferences(
     query_fraction=1.0,
     num_samples=1,
     measures=[],  # default: all measures in the preferences file
+    nb_queries = None # NOTE: added myself, keep only first nb_queries queries
 ):
     """
     Main of pref_aggregate adapted for usage in notebooks. Preferences are loaded separately.
@@ -149,6 +190,12 @@ def aggregate_preferences(
     """
     system_orderings_by_query = []
     system_orderings = {}
+    
+    qids_to_keep = sorted(list({pref["qid"] for pref in prefs if pref["qid"] != "all"}))[:nb_queries] + ["all"] if nb_queries is not None else None
+    
+    if qids_to_keep is not None:
+        prefs: Preferences = [pref for pref in prefs if pref["qid"] in qids_to_keep]
+    
     qids = list({pref["qid"] for pref in prefs}) if (query_fraction < 1.0) else None
 
     sample_size = (
@@ -177,7 +224,6 @@ def aggregate_preferences(
 
         p_rankings: Rankings = get_query_rankings_from_preferences(prefs)
         m_rankings: Rankings = get_query_rankings_from_metrics(metrics)
-
         if query_eval_wanted:
             for qid in sample_qids:
                 output_object = {}
