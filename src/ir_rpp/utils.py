b8cd4ca79d608df298af0468ff8c71fab91c8b8c
sitaraliang <sitaraliang@gmail.com>
2026-05-15 16:19:06 +0200

    run subtopics for all web dataset & filter queries?

--- src/ir_rpp/utils.py
+++ src/ir_rpp/utils.py
@@ -4,3 +4,13 @@ def get_shape(d):
         shape.append(len(d))
         d = next(iter(d.values()))
     return tuple(shape)
+
+def filter_valid_queries(df_metric, df_pref):
+    # queries where the mean score of any metric is 0 
+    valid_qids = df_metric.groupby("qid")["map-ia"].mean()
+    valid_qids = valid_qids[valid_qids > 0].index.tolist()
+    
+    df_metric_filtered = df_metric[df_metric["qid"].isin(valid_qids)]
+    df_pref_filtered = df_pref[df_pref["qid"].isin(valid_qids)]
+    
+    return df_metric_filtered, df_pref_filtered
